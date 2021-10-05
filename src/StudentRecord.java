d5cdc820767ef393b65938bfbb1d5a99abf70bb2
Arijit Chabri <arijitchabri2001@gmail.com>
2021-10-05 10:11:04 +0530

    Optimized a little
    Added Binary Search, Sorted the database after each addition or update.

--- src/StudentRecord.java
+++ src/StudentRecord.java
@@ -14,6 +14,23 @@ public class StudentRecord {
     static String[] names =new String[num];
     static int[] rollNumber = new int[num];
 
+    public static int binarySearch(int element, int low, int high){
+        int mid = (high + low)/2;
+        if(low >= high){
+           return -1;
+        }
+        else if(rollNumber[mid] == element){
+            return mid;
+        }
+        else if(rollNumber[mid] > element){
+             return binarySearch(element, low, mid);
+        }
+        else{
+            return binarySearch(element, mid+1, high);
+        }
+
+    }
+
     public static void printStudent(int index){
         System.out.println(       "Name        Roll");
         System.out.println(names[index] + "        " + rollNumber[index]);
@@ -26,12 +43,33 @@ public class StudentRecord {
         }
     }
 
-    // TODO :- Have to create a binary searchable pattern for adding items.
-    /*
-    We will use insertion sorting at here for sorting the record roll wise
-    and we have to modify the sort for make it work along with the names also
-    so we can find the proper name and roll number at same index.
-    */
+    public static void swap(int temp){
+        int rtemp = rollNumber[temp];
+        String ntemp = names[temp];
+        rollNumber[temp] = rollNumber[temp - 1];
+        names[temp] = names[temp - 1];
+        rollNumber[temp - 1] = rtemp;
+        names [temp - 1] = ntemp;
+    }
+
+
+    public static void sort(int pos){
+        int temp = pos;
+        if (index > 0){
+            try{
+                while(rollNumber[temp] < rollNumber[temp-1]){
+                    swap(temp);
+                    temp--;
+                }
+            }
+            catch (ArrayIndexOutOfBoundsException e){
+
+            }
+
+        }
+
+    }
+
 
     public static void addStudent(){
         System.out.println("Student name :- ");
@@ -41,33 +79,34 @@ public class StudentRecord {
         int roll = sc.nextInt();
         names[index] = name;
         rollNumber[index] = roll;
+        sort(index);
         index++;
         System.out.println("Student added successfully");
     }
-    public static void updateStudent(int index){
+    public static void updateStudent(int pos){
         sc.nextLine();
         System.out.println("Student name :- ");
         String name = sc.nextLine();
         System.out.println("Student roll :- ");
         int roll = sc.nextInt();
-        names[index] = name;
-        rollNumber[index] = roll;
+        names[pos] = name;
+        rollNumber[pos] = roll;
+        sort(pos);
+
     }
 
     public static void update(){
-        boolean flag = false;
-        System.out.println("Roll of the student you want to update details.");
-        int stu = sc.nextInt();
-        //TODO :- At here we will call the binary search function and pass the returned value to the updateStudent() function
 
-        for (int i = 0; i < index; i++) {
-            if (rollNumber[i] == stu) {
-                updateStudent(i);
-                flag = true;
-                System.out.println("Student record Updated.");
-                break;
-            }
+        boolean flag = false;
+        System.out.println("Roll of the student you want to update details :- ");
+        int stu = sc.nextInt();
+        int pos = binarySearch(stu, 0, index);
+        if (pos != -1) {
+            updateStudent(pos);
+            flag = true;
+            System.out.println("Student record Updated.");
         }
+
         if (!flag){
             System.out.println("""
                     Sorry there is no such student in the records.\s
@@ -107,36 +146,39 @@ public class StudentRecord {
             System.out.println("press 6 for help ! \n What is your choice :- ");
             choice = sc.nextInt();
             switch (choice) {
-                case 1 :{
+                case 1 : {
                     addStudent();
                     break;
                 }
-                case 2 :{
+                case 2 : {
                     System.out.println("Give me the index number :- ");
                     printStudent(sc.nextInt());
                     break;
                 }
-                case 3 :{
+                case 3 : {
+
                     printStudents();
                     break;
                 }
-                case 4:{
+                case 4: {
+
                     update();
                     break;
                 }
-                case 5 :{
+                case 5 : {
                     System.exit(0);
                     break;
                 }
-                case 6 :{
+                case 6 : {
                     System.out.println("""
                             1. For adding a student
                             2. For printing a particular Student
                             3. For printing the whole Student record
-                            4. For exiting""");
+                            4. For updating a student record
+                            5. For exiting""");
                     break;
                 }
-                default :{
+                default : {
                     System.out.println("Wrong input please try again !");
                 }
             }
--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,64 @@
-import java.util.Scanner;
-import java.lang.String;
+class Test{
 
-public class Test {
-    static Scanner sc = new Scanner(System.in);
-    static void fun(){
-        int i = sc.nextInt();
-        String name = sc.nextLine();
-
-        System.out.println(name + "       " + i);
+    public static void print(int []arr){
+        System.out.println();
+        for (int j : arr) {
+            System.out.print(j + "   ");
+        }
     }
+
+    public static void merge(int []arr, int []left, int []right, int l, int r){
+        int i = 0, j = 0, k = 0;
+        while(i < l && j < r){
+            if(left[i] > right[j]){
+                arr[k++] = right[j++];
+            }
+            else{
+                arr[k++] = left[i++];
+            }
+        }
+        while(i < l){
+            arr[k++] = left[i++];
+        }
+        while(j < r){
+            arr[k++] = right[j++];
+        }
+
+    }
+
+    public static void sort(int []arr){
+        int len = arr.length;
+        if (len < 2){
+            return;
+        }
+        int mid = (len+1)/2;
+        int []left = new int[mid];
+        int []right = new int[len-mid];
+        try{
+            for (int i = 0; i <= mid; i++){
+                left[i] = arr[i];
+                right[i] = arr[i+mid];
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+
+        }
+        sort(left);
+        sort(right);
+        merge(arr, left, right, mid, len-mid);
+    }
+
+
+
     public static void main(String[] args) {
-        fun();
+        int n = 23;
+        String s = "";
+        s = s.format("%" + (-20) + "s", "Arijit");
+        System.out.println(s + n);
+        s = s.format("%" + (-20) + "s", "Arijit Chabri");
+        System.out.println(s + n);
+
+
     }
+
 }
